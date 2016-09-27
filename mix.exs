2f93916078a6e47c93a6d810cab30b3c77ad435e
Josh Austin <josh.austin@gmail.com>
2016-09-27 14:59:37 -0400

    update version to 0.0.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExRedisPool.Mixfile do
 
   def project do
     [app: :ex_redis_pool,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
