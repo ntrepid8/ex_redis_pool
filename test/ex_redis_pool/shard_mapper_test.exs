a113025007371a95c0653d249f556a543002ff22
Josh Austin <josh.austin@gmail.com>
2016-11-30 15:47:04 -0500

    update shard mapper test

--- test/ex_redis_pool/shard_mapper_test.exs
+++ test/ex_redis_pool/shard_mapper_test.exs
@@ -2,15 +2,65 @@ defmodule ExRedisPool.ShardMapperTest do
   use ExUnit.Case, async: true
 
   test "ExRedisPool.ShardMapper.q/3 [basic operation]" do
-    # start shards
-    {:ok, rp0_pid} = ExRedisPool.RedisPool.start_link(database: 10)
-    {:ok, rp1_pid} = ExRedisPool.RedisPool.start_link(database: 11)
-    {:ok, rp2_pid} = ExRedisPool.RedisPool.start_link(database: 12)
-    {:ok, rp3_pid} = ExRedisPool.RedisPool.start_link(database: 13)
+    # shard connection options
+    shard_opts_list = [
+      [database: 10],
+      [database: 11],
+      [database: 12],
+      [database: 13],
+    ]
 
-    # start shard mapper
-    shards = [rp0_pid, rp1_pid]
-    {:ok, pid} = ExRedisPool.ShardMapper.start_link(shards)
+    {:ok, pid} = ExRedisPool.Shard.new(shard_opts_list)
+
+    # run a few iterations to make sure we distribute across the shards
+    for _ <- 0..100 do
+      shard_key = "#{:crypto.rand_uniform(0, 1_000_000_000)}"
+      key =       "test_#{:crypto.rand_uniform(0, 1_000_000_000)}"
+      val =       "#{:crypto.rand_uniform(0, 1_000_000_000)}"
+
+      {:ok, "OK"} = ExRedisPool.Shard.q(pid, ["SET", key, val], shard_key, 5_000)
+      {:ok, result} = ExRedisPool.Shard.q(pid, ["GET", key], shard_key, 5_000)
+      assert result == val
+      {:ok, result} = ExRedisPool.Shard.q(pid, ["DEL", key], shard_key, 5_000)
+      assert result == "1"
+    end
+  end
+
+  test "ExRedisPool.ShardMapper.q/3 [basic operation (reuse existing connections)]" do
+    # this test might actually run before the [basic operation] test, but the
+    # connections should only be created once.
+    # shard connection options
+    shard_opts_list = [
+      [database: 10],
+      [database: 11],
+      [database: 12],
+      [database: 13],
+    ]
+
+    {:ok, pid} = ExRedisPool.Shard.new(shard_opts_list)
+
+    # run a few iterations to make sure we distribute across the shards
+    for _ <- 0..100 do
+      shard_key = "#{:crypto.rand_uniform(0, 1_000_000_000)}"
+      key =       "test_#{:crypto.rand_uniform(0, 1_000_000_000)}"
+      val =       "#{:crypto.rand_uniform(0, 1_000_000_000)}"
+
+      {:ok, "OK"} = ExRedisPool.Shard.q(pid, ["SET", key, val], shard_key, 5_000)
+      {:ok, result} = ExRedisPool.Shard.q(pid, ["GET", key], shard_key, 5_000)
+      assert result == val
+      {:ok, result} = ExRedisPool.Shard.q(pid, ["DEL", key], shard_key, 5_000)
+      assert result == "1"
+    end
+  end
+
+  test "ExRedisPool.ShardMapper.q/3 [named shard mapper]" do
+    # shard connection options
+    shard_opts_list = [
+      [database: 14],
+      [database: 15],
+    ]
+
+    {:ok, pid} = ExRedisPool.Shard.new(:test_shard_map, shard_opts_list)
 
     # run a few iterations to make sure we distribute across the shards
     for _ <- 0..10 do
@@ -18,10 +68,10 @@ defmodule ExRedisPool.ShardMapperTest do
       key =       "test_#{:crypto.rand_uniform(0, 1_000_000_000)}"
       val =       "#{:crypto.rand_uniform(0, 1_000_000_000)}"
 
-      {:ok, "OK"} = ExRedisPool.ShardMapper.q(pid, ["SET", key, val], shard_key, 5_000)
-      {:ok, result} = ExRedisPool.ShardMapper.q(pid, ["GET", key], shard_key, 5_000)
+      {:ok, "OK"} = ExRedisPool.Shard.q(:test_shard_map, ["SET", key, val], shard_key, 5_000)
+      {:ok, result} = ExRedisPool.Shard.q(:test_shard_map, ["GET", key], shard_key, 5_000)
       assert result == val
-      {:ok, result} = ExRedisPool.ShardMapper.q(pid, ["DEL", key], shard_key, 5_000)
+      {:ok, result} = ExRedisPool.Shard.q(:test_shard_map, ["DEL", key], shard_key, 5_000)
       assert result == "1"
     end
   end
